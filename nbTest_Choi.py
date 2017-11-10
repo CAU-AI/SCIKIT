94651e5688e925395671086b979b6c3c9e4e896b
hytae1993 <hytae1993@hanmail.net>
2017-11-10 21:10:54 +0900

    PCA적용 한 data와 적용 안한 data를 test_size변화시키면서 Naive_Bayes test

---
+++ nbTest_Choi.py
@@ -0,0 +1,55 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import PCA as pca
+
+#test_size 별 accuracy 측정
+
+xl = pd.ExcelFile("Breastcancer_train.xlsx")
+data_excel = pd.read_excel(io=xl, sheetname=0, header=None)
+answer_excel = pd.read_excel(io=xl, sheetname=1, header=None)
+data = np.array(data_excel.values)
+answer = np.array(answer_excel.values).flatten().transpose()
+
+range_list = np.arange(0.1, 1, 0.1)
+max_accuracy = 0
+max_test_size = 0
+
+print("PCA미적용 nb")
+for i in range_list:
+    train_data, test_data, train_answer, test_answer = train_test_split(data, answer, test_size=i)
+    gnb = GaussianNB()
+    train_model = gnb.fit(train_data, train_answer)
+    test_pred = train_model.predict(test_data)
+    correct_count = (test_pred == test_answer).sum()
+    accuracy = correct_count / len(test_answer)
+    if max_accuracy < accuracy:
+        max_accuracy = accuracy
+        max_test_size = i
+    print("(test_size : " + str(i) + ") 일치하는 data 갯수 = " + str(correct_count) + " test_data 갯수 = " + str(len(test_data))+ "\t\tAccuracy = " + str(accuracy))
+
+
+print("\nPCA적용 nb")
+max_pca_accuracy = 0
+max_pca_test_size = 0
+
+data_PCA = np.array(pca.new_data1)
+data_PCA = np.c_[data_PCA,pca.new_data2]
+
+for i in range_list:
+    train_data, test_data, train_answer, test_answer = train_test_split(data_PCA, answer, test_size=i)
+    gnb = GaussianNB()
+    train_model = gnb.fit(train_data, train_answer)
+    test_pred = train_model.predict(test_data)
+    correct_count = (test_pred == test_answer).sum()
+    accuracy = correct_count / len(test_answer)
+    if max_pca_accuracy < accuracy:
+        max_pca_accuracy = accuracy
+        max_pca_test_size = i
+    print("(test_size : " + str(i) + ") 일치하는 data 갯수 = " + str(correct_count) + " test_data 갯수 = " + str(
+        len(test_data)) + "\t\tAccuracy = " + str(accuracy))
+
+print("\nPCA미적용 : Max_accuracy's test_size = " + str(max_test_size) + "   MAX_accuracy = " + str(max_accuracy))
+print("PCA적용 : Max_accuracy's test_size = " + str(max_pca_test_size) + "   Max_accuracy = " + str(max_pca_accuracy))
+
