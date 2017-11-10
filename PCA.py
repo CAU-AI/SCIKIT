a83e89bf8cac207e14f2ac6de9f9d2272658becf
hytae1993 <hytae1993@hanmail.net>
2017-11-10 17:56:38 +0900

    하고나니까 왜했는지 모르겠네요

--- PCA.py
+++ PCA.py
@@ -12,16 +12,14 @@ answer_excel = pd.read_excel(io=xl, sheetname=1, header=None)
 data = np.array(data_excel.values)              #583*9
 answer = np.array(answer_excel.values).flatten().transpose()
 
-
-cov_data = np.cov(data)      #583 * 583
-
-eigenvalue = np.linalg.eigvals(cov_data)     #583
+cov_data = np.cov(data.T)      #9*9
+eigenvalue = np.linalg.eigvals(cov_data)     #9
 
 a=eigenvalue.tolist()
 b=eigenvalue.tolist()
 
-'''
-a.sort(reverse=True)                    #sort에 오류발생
+
+a.sort(reverse=True)
 
 first_eigenvector_index = b.index(a[1])
 second_eigenvector_index = b.index(a[1])
@@ -30,19 +28,20 @@ third_eigenvector_index = b.index(a[2])
 first_eigenvector_index = 0
 second_eigenvector_index = 1
 third_eigenvector_index =  2
-
+'''
 first_eigenvector = np.empty(1)
 second_eigenvector = np.array(1)
 third_eigenvector = np.empty(1)
 
 
 for i in range(0, len(cov_data)):
-    first_eigenvector = np.c_[first_eigenvector, [cov_data[i][first_eigenvector_index]]]       #1*583
+    first_eigenvector = np.c_[first_eigenvector, [cov_data[i][first_eigenvector_index]]]       #1*10
     second_eigenvector = np.c_[second_eigenvector, [cov_data[i][second_eigenvector_index]]]
     third_eigenvector = np.c_[third_eigenvector, [cov_data[i][third_eigenvector_index]]]
 
+
 # 42~48 : 첫번째 index 0을 지운후 다시 array로 설정 -> 빈 ndarray 생성이 안돼 추가했습니다.
-first_list = first_eigenvector.tolist()
+first_list = first_eigenvector.tolist()     #1*9
 second_list = second_eigenvector.tolist()
 third_list = third_eigenvector.tolist()
 
@@ -55,10 +54,10 @@ first_eigenvector = np.array(first_list).T
 second_eigenvector = np.array(second_list).T
 third_eigenvector = np.array(third_list).T
 
-new_data1 = np.dot(data,first_eigenvector)
-new_data2 = np.dot(data,second_eigenvector)
-new_data3 = np.dot(data,third_eigenvector)
 
+new_data1 = np.dot(data,first_eigenvector)
+new_data2 = np.dot(data,second_eigenvector)             #583*1
+new_data3 = np.dot(data,third_eigenvector)
 
 
 
@@ -70,9 +69,12 @@ ax = fig.add_subplot(111,projection='3d')
 for i in range(0, new_data1.size):
     x=new_data1[i][0]
     y=new_data2[i][0]
-    z=new_data3[i][0]
-    ax.scatter(x, y, z)
-
+    #z=new_data3[i][0]
+    z=np.array(answer)[i]
+    if z == 1:
+        ax.scatter(x, y, z, edgecolors='red')
+    else :
+        ax.scatter(x, y, z, edgecolors='blue')
 plt.show()
 
 
