52a1674891ba8db68dfc6e1f991dbf383f8c4d36
Velmont <dragond7@naver.com>
2017-11-30 16:11:04 +0900

    lda 마무리

--- nbTest.py
+++ nbTest.py
@@ -12,55 +12,58 @@ f = open('result1.txt', 'w+t')
 xl = pd.ExcelFile("Breastcancer_train.xlsx")
 # xl = pd.ExcelFile("Music_style_train.xlsx")
 demo_xl = pd.ExcelFile("demo_test.xlsx")
+
 data_excel = pd.read_excel(io=xl, sheetname=0, header=None)
 answer_excel = pd.read_excel(io=xl, sheetname=1, header=None)
 data = np.array(data_excel.values)
 answer = np.array(answer_excel.values).flatten().transpose()
 
-data = np.array(data_excel.values)
-answer = np.array(answer_excel.values).flatten().transpose()
-
 demo_excel = pd.read_excel(io=demo_xl, sheetname=0, header=None)
 answer_demo_excel = pd.read_excel(io=demo_xl, sheetname=1, header=None)
+demo_data = np.array(demo_excel.values)
+#demo_answer = np.array(answer_demo_excel.values).flatten().transpose()
 
-train_data, test_data, train_answer, test_answer = train_test_split(data, answer, test_size=0.01)
-
+train_data, test_data, train_answer, test_answer = train_test_split(data, answer, test_size=0.2)
+train_data = data
+train_answer = answer
 
 # Standardzation before training
 scaler = preprocessing.Imputer(missing_values='NaN', strategy='median').fit(train_data.astype(float))
 train_data = scaler.transform(train_data.astype(float))
 test_data = scaler.transform(test_data.astype(float))
+#demo_data = scaler.transform(demo_data.astype(float))
 
-gnb = GaussianNB()
-train_model = gnb.fit(train_data, train_answer)
-test_pred1 = train_model.predict(test_data)
+#gnb = GaussianNB()
+#train_model = gnb.fit(train_data, train_answer)
+#test_pred1 = train_model.predict(demo_data)
 
-correct_count = (test_pred1 == test_answer).sum()
-accuracy1 = correct_count / len(test_answer)
-print("Accuracy = " + str(accuracy1))
+#correct_count = (test_pred1 == demo_answer).sum()
+#accuracy1 = correct_count / len(demo_answer)
+#print("Accuracy = " + str(accuracy1))
 
-feature_size = 5
-pca = LDA(n_components=feature_size)
+feature_size = 3
+lda = LDA(n_components=feature_size)
 
-train_data_proc = pca.fit_transform(train_data, train_answer)
-test_data_proc = pca.fit_transform(test_data, test_answer)
+train_data_proc = lda.fit(train_data, train_answer)
+#test_data_proc = lda.fit_transform(test_data, test_answer)
+#demo_data_proc = lda.fit_transform(demo_data, demo_answer)
 
+#print("train_data_proc : ")
+#print( np.array(train_data_proc).shape)
 
-print("train_data_proc : ")
-print( np.array(train_data_proc).shape)
+#gnb = GaussianNB()
+#train_model = gnb.fit(train_data_proc, train_answer)
+test_pred2 = train_data_proc.predict(demo_data)
 
-gnb = GaussianNB()
-train_model = gnb.fit(train_data_proc, train_answer)
-test_pred2 = train_model.predict(test_data_proc)
+#correct_count = (test_pred2 == demo_answer).sum()
+#accuracy2 = correct_count / len(demo_answer)
+#print("Accuracy proc= " + str(accuracy2))
 
-correct_count = (test_pred2 == test_answer).sum()
-accuracy2 = correct_count / len(test_answer)
-print("Accuracy proc= " + str(accuracy2))
-
-if accuracy1 > accuracy2:
-    demo_pred = test_pred1
-else:
-    demo_pred = test_pred2
+#if accuracy1 > accuracy2:
+#    demo_pred = test_pred1
+#else:
+demo_pred = test_pred2
 
 for i in demo_pred:
     f.write(str(i) + '\n')
+print("finish")
--- nbTest_orig.py
+++ nbTest_orig.py
@@ -2,21 +2,75 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
+xl = pd.ExcelFile("Breastcancer_train.xlsx")
+# xl = pd.ExcelFile("Music_style_train.xlsx")
+demo_xl = pd.ExcelFile("demo_test.xlsx")
 
-xl = pd.ExcelFile("zoo.xlsx")
+f = open('result1.txt', 'w+t')
+
 data_excel = pd.read_excel(io=xl, sheetname=0, header=None)
 answer_excel = pd.read_excel(io=xl, sheetname=1, header=None)
 data = np.array(data_excel.values)
 answer = np.array(answer_excel.values).flatten().transpose()
 
+demo_excel = pd.read_excel(io=demo_xl, sheetname=0, header=None)
+answer_demo_excel = pd.read_excel(io=demo_xl, sheetname=1, header=None)
 
-train_data, test_data, train_answer, test_answer = train_test_split(data, answer, test_size=0.2)
+ldacnt =0;
+nbcnt =0;
+ldasum = 0
+nbsum = 0
+for i in range(0,50000):
+    train_data, test_data, train_answer, test_answer = train_test_split(data, answer, test_size=0.2)
 
-gnb = GaussianNB()
-train_model = gnb.fit(train_data, train_answer)
-test_pred = train_model.predict(test_data)
+    # Standardzation before training
+    scaler = preprocessing.Imputer(missing_values='NaN', strategy='median').fit(train_data.astype(float))
+    train_data = scaler.transform(train_data.astype(float))
+    test_data = scaler.transform(test_data.astype(float))
 
-correct_count = (test_pred == test_answer).sum()
-accuracy = correct_count / len(test_answer)
-print("Accuracy = " + str(accuracy))
+    gnb = GaussianNB()
+    train_model = gnb.fit(train_data, train_answer)
+    test_pred1 = train_model.predict(test_data)
+
+    correct_count = (test_pred1 == test_answer).sum()
+    accuracy1 = correct_count / len(test_answer)
+    #print("Accuracy = " + str(accuracy1))
+
+    feature_size = 3
+    lda = LDA(n_components=feature_size)
+    pca = PCA(n_components=feature_size)
+
+    train_data_proc = lda.fit(train_data, train_answer)
+    # test_data_proc = pca2.fit_transform(test_data)
+
+    #print("train_data_proc : ")
+    #print( np.array(train_data_proc).shape)
+
+    # gnb = GaussianNB()
+    # train_model = gnb.fit(train_data_proc, train_answer)
+    # test_pred2 = train_model.predict(test_data_proc)
+    test_pred2 = train_data_proc.predict(test_data)
+
+    correct_count = (test_pred2 == test_answer).sum()
+    accuracy2 = correct_count / len(test_answer)
+    #print("Accuracy proc= " + str(accuracy2))
+
+    nbsum += accuracy1
+    ldasum += accuracy2
+    if accuracy1 > accuracy2:
+        demo_pred = test_pred1
+        nbcnt+=1
+    else:
+        demo_pred = test_pred2
+        ldacnt+=1
+    for i in demo_pred:
+        f.write(str(demo_pred[i]) + '\n')
+
+print("lda " + str(ldacnt))
+print("acc " + str(ldasum/50000))
+print("nb " + str(nbcnt))
+print("acc "  +str(nbsum/50000))
